82df156365e1c603b695b8dfb1817659f1d432bd
Val <valmack@protonmail.com>
2020-04-12 10:53:50 -0400

    Add audio routes

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,16 @@ var router = express.Router();
 
 const UPLOAD_PATH = '/Users/val/liveset-backend/public/uploads/';
 
-/* GET latest audio file. */
 router.get('/', function(req, res, next) {
+  res.send("Alive");
+});
+
+/* GET latest audio file. */
+router.get('/audio', function(req, res, next) {
   res.sendFile(UPLOAD_PATH + 'latest.webm');
 });
 
-router.post('/', upload.single('soundBlob'), function (req, res, next) {
+router.post('/audio', upload.single('soundBlob'), function (req, res, next) {
   console.log(req.file)
   fs.writeFileSync(UPLOAD_PATH + 'latest.webm', Buffer.from(new Uint8Array(req.file.buffer)));
   res.sendStatus(200);
